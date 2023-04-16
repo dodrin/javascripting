4cb9a525e8d1c9b505bf9564491cc779c3a61e51
Rina Inada <indrn@icloud.com>
2023-04-16 21:25:47 +0000

    Initial commit with all my work so far

---
+++ arrays.js
@@ -0,0 +1,12 @@
+/* In that file define a variable named pizzaToppings that references an  
+  array that contains three strings in this order: tomato sauce, cheese,  
+  pepperoni.  
+   
+  Use console.log() to print the pizzaToppings array to the terminal.  
+   
+  Check to see if your program is correct by running this command:  
+   
+     javascripting verify arrays.js */
+
+const pizzaToppings = ["tomato sauce", "cheese", "pepperoni"];
+console.log(pizzaToppings);
